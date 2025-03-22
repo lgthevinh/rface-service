031ba12256b8f3b8db0bc6a540be3b6b9685f45a
lgthvinh <everwellmax@gmail.com>
2025-03-22 23:51:37 +0700

    refactor main

--- app/main.py
+++ app/main.py
@@ -1,17 +1,6 @@
-import os
-
 from flask import Flask, jsonify
 from api.routes import api_blueprint
-from services.face_recognition import FaceRecognition
-
-data_path = "../data/"
-if not os.path.exists(data_path):
-  os.makedirs(data_path)
-    
-# Check DEEPFACE_HOME environment variable
-if "DEEPFACE_HOME" not in os.environ:
-  os.makedirs("./data/.deepface/weights", exist_ok=True)
-  os.environ["DEEPFACE_HOME"] = data_path
+from app_config import AppConfig
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint)
@@ -20,8 +9,6 @@ app.register_blueprint(api_blueprint)
 def ping():
   return jsonify({"message": "Pong! RFace Service is here!"}), 200
 
-if __name__ == "__main__":  
-  FaceRecognition().set_model("Dlib")  
-  FaceRecognition().set_threshold(0.04)
-  
+if __name__ == "__main__":
+  AppConfig().init()
   app.run(host="0.0.0.0", port="2248", debug=True)
