1dfe38a65c991e9cbade64949d5d1926f9119ab9
lgthvinh <everwellmax@gmail.com>
2025-03-27 12:01:55 +0700

    add to_dict method for camera class

--- app/models/camera.py
+++ app/models/camera.py
@@ -1,8 +1,15 @@
 class Camera:
-  def __init__(self, name: str, rtsp_url: str):
+  def __init__(self, name: str, rtsp_url: str, id: int = None):
     self.name = name
     self.rtsp_url = rtsp_url
-    self._id = None
+    self.id = id
   
   def __str__(self):
     return f"Camera({self.name}, {self.rtsp_url})"
+  
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "name": self.name,
+      "rtsp_url": self.rtsp_url
+    }
