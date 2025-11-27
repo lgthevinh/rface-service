26dcefda6c1ba36ace68428d887b8114d87eeb84
lgthvinh <everwellmax@gmail.com>
2025-11-28 01:06:14 +0700

    enable logging and add initialization log in RFaceService

--- src/main/java/org/thingai/app/Main.java
+++ src/main/java/org/thingai/app/Main.java
@@ -1,11 +1,16 @@
 package org.thingai.app;
 
+import org.thingai.base.log.ILog;
+
 public class Main {
     public static void main(String[] args) {
         RFaceService service = new RFaceService();
         service.name = "RFace Service";
         service.version = "1.0.0";
         service.appDirName = "rface_app";
+
+        ILog.ENABLE_LOGGING = true;
+        ILog.logLevel = ILog.DEBUG;
 
         service.init();
     }
--- src/main/java/org/thingai/app/RFaceService.java
+++ src/main/java/org/thingai/app/RFaceService.java
@@ -4,6 +4,7 @@ import org.thingai.base.Service;
 import org.thingai.base.dao.Dao;
 import org.thingai.base.dao.DaoFile;
 import org.thingai.base.dao.DaoSqlite;
+import org.thingai.base.log.ILog;
 
 public class RFaceService extends Service {
     private Dao dao;
@@ -13,5 +14,7 @@ public class RFaceService extends Service {
     protected void onServiceInit() {
         dao = new DaoSqlite(appDir + "/rface.db");
         daoFile = new DaoFile(appDir + "/files");
+
+        ILog.d("RFaceService", "Service initialized with DAO and file storage.");
     }
 }
