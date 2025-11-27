8d8b13a4cfe08907cfe9f4c7608d13a682b415a1
lgthvinh <everwellmax@gmail.com>
2025-11-28 01:02:11 +0700

    add dependencies and remove redundant comments in Main

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,11 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
+    implementation("org.xerial:sqlite-jdbc:3.43.2.0")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("com.google.code.gson:gson:2.13.2")
+
     implementation(fileTree("libs") {
         include("*.jar")
     })
