b4a9def97becdd11663dac82cd2de61515ebf032
lgthvinh <everwellmax@gmail.com>
2025-12-05 01:34:14 +0700

    add DaoEmbedding annotation for embedding DAO classes

---
+++ aibase/src/main/java/org/thingai/base/ai/vector/dao/DaoEmbedding.java
@@ -0,0 +1,11 @@
+package org.thingai.base.ai.vector.dao;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DaoEmbedding {
+}
